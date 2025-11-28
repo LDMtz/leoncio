55f45b7334c302940c265766c288209f8fb350c3
Leoncio Martinez <leonciodanielmartinez@gmail.com>
2025-11-28 06:05:41 -0700

    Inicialización de tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 
 function App() {
   return (
-    <h1>Trabajando en el portafolio, disponible proximamente ...</h1>
+    <h1 className='text-blue-400 font-bold text-2xl'>Trabajando en el portafolio, disponible proximamente ...</h1>
   )
 }
 
